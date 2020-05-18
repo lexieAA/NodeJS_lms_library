9cb7a22b22a9e7ec237710b30f353f357efbd595
lexieAA <43833670+lexieAA@users.noreply.github.com>
2020-05-17 21:19:43 -0700

    GetBookCopiesById now shows book title and branch name

--- lms_library/dao/BookCopiesDao.js
+++ lms_library/dao/BookCopiesDao.js
@@ -10,7 +10,7 @@ exports.getBookCopiesByBranchId = function (branchId) {
 
 exports.getBookCopiesById = function (branchId, bookId) {
     return new Promise(function (resolve, reject) {
-        db.query('select * from library.tbl_book_copies where branchId = ? and bookId = ?', [branchId, bookId], function (err, result) {
+        db.query('SELECT tb.bookId AS bookId, tb.title AS title, tbc.branchId As branchId, tlb.branchName As branchName,tbc.noOfCopies As noOfCopies FROM tbl_book_copies  As tbc INNER JOIN tbl_book AS tb on tbc.bookId = tb.bookId INNER JOiN tbl_library_branch AS tlb ON tbc.branchId = tlb.branchId WHERE tlb.branchId = ? AND tbc.bookId = ?', [branchId, bookId], function (err, result) {
             return err ? reject(err) : resolve(result);
         });
     });;
